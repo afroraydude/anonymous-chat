a7fca22202ab7f9aa156daf1f76919cf549d5946
root <root@afroraydude.com>
2018-02-02 02:05:46 +0000

    v2 server # FEATURES * send messages encrypted with unique key * server decrypts the message then sends the decrypted message to all sockets # PLANS * send messages encrypted to each and every socket with their unique socket encryption key

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app = https.createServer(httpsoptions);
 io = require('socket.io',{secure: true})(app);
 require('dotenv').config();
 var crypto = require('crypto');
+var algorithm = 'aes-256-ctr';
 sockets = [];
 messages = [];
 var Realm = require('realm');
@@ -22,31 +23,51 @@ const MessageSchema = {
   }
 }
 
-var serverInfo = {version: "1", title: "Test Server", rooms: ['/']};
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 15; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+var serverInfo = {version: "2", title: "Test Server", rooms: ['/']};
 
 io.on('connection', (socket) => {
   console.log("new socket with id "+socket.id+" has connected and is ready to recieve messages.");
   var x = crypto.createHash('md4').update(socket.id).digest("hex");
   console.log("socket with id "+socket.id+"has been sent it's anonid which is "+x);
   socket.name = String(x);
+  socket.crypto = makeid();
+  console.log(socket.crypto);
   sockets.push(socket);
   var colorChoice = colors[Math.floor(Math.random() * colors.length)];
-
-  socket.emit("identification", {id: x, color: colorChoice});
+  socket.emit("identification", {id: x, color: colorChoice, crypto: socket.crypto});
   socket.emit("messagelist", messages);
   socket.emit("message", {client: "Server", color: "red", data: "Welcome!"});
   socket.emit("version", serverInfo.version);
   socket.emit("serverinfo", serverInfo);
-
   socket.on('disconnect', function () {
     sockets.splice(sockets.indexOf(socket), 1);
     console.log('client left');
   });
   socket.on('message', function(message) {
     messages.push(message);
-    if (messages.length > 10) {
+    if (messages.length > process.env.maxmsg) {
       messages.shift();
     }
+    var xx = crypto.createDecipher(algorithm,socket.crypto);
+    var yy = xx.update(message.data, 'hex', 'utf8');
+    message.data = yy;
+    let realm = new Realm({schema: [MessageSchema]});
+    realm.write(() => {
+        let x = realm.create('Message', {
+            client: message.client,
+            color: message.color,
+            data: yy
+        });
+    });
     io.emit("message", message);
     console.log("sent a message");
   });
@@ -59,3 +80,7 @@ setInterval(function () {
 const port = process.env.port || 1234;
 app.listen(port);
 console.log('listening on port ', port);
+
+process.on('uncaughtException', function (err) {
+      console.log('Caught exception: ', err);
+});
